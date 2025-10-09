2bfffb4d8b276af51699d85aae6f15af0c53c3ee
dimasbagas <danibaret014@gmail.com>
2025-10-09 11:51:42 +0700

    test2

--- components/pinjaman-terkini/ComponentPinjaman.jsx
+++ components/pinjaman-terkini/ComponentPinjaman.jsx
@@ -8,6 +8,7 @@ const ComponentPinjaman = () => {
     fetchLoans();
   }, []);
 
+  
   const fetchLoans = async () => {
     try {
       const response = await fetch('https://0b8763b66148.ngrok-free.app/api/biblio/');
